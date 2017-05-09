10f9585071b1b86b7588cee171c807f8ffe0f0ed
Rodrigo Ramele <rramele@gmail.com>
2017-05-08 23:33:08 -0300

    Update Isafe.m

--- Isafe.m
+++ Isafe.m
@@ -1,3 +1,4 @@
+% Asigna el valor "val" a la imagen I() pero chequeando los limites (i,j) para que no se pasen (x eso es SAFE)
 function I= Isafe(I,i,j,val)
 if (i>0 && i<=size(I,1) && j>0 && j<=size(I,2))
     I(i,j) = val;
